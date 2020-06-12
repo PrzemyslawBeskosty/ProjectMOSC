8f6bc75ef2d895f89b3db047781fa1eb9542db49
Przemyslaw Beskosty <beskosty@gmail.com>
2020-06-12 17:09:57 +0200

    Unit test for Client. Test for getStatusBundled() method.

--- MOSC/src/test/java/pl/beskosty/mosc/entity/TestClient.java
+++ MOSC/src/test/java/pl/beskosty/mosc/entity/TestClient.java
@@ -1,8 +1,12 @@
 package pl.beskosty.mosc.entity;
 
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import pl.beskosty.mosc.enums.Status;
 
 /**
  *
@@ -43,6 +47,44 @@ public class TestClient {
         boolean isActive = client.isActive();
         //Then
         assertTrue(isActive);
+    }
+
+    @Test
+    public void checkNullOnStatusBundledOnConstructor() {
+        //Given
+        Client client = new Client();
+        //Then
+        assertNotEquals(null, client.getStatusBundled());
+    }
+
+    @Test
+    public void checkStatusBundledOnConstructor() {
+        //Given
+        Client client = new Client();
+        //Then
+        assertNotEquals(Status.ACTIVE.getValue(), client.getStatusBundled());
+    }
+
+    @Test
+    public void checkStatusBundledWithIncorrectStatus() {
+        //Given
+        Client client = new Client();
+        //When
+        client.setStatus("ANY STATUS HERE?");
+        boolean isActive = client.isActive();
+        //Then
+        assertNotEquals(Status.ACTIVE.getValue(), client.getStatusBundled());
+    }
+
+    @Test
+    public void checkStatusBundledWithCorrectStatus() {
+        //Given
+        Client client = new Client();
+        //When
+        client.setStatus("A");
+        boolean isActive = client.isActive();
+        //Then
+        assertEquals(Status.ACTIVE.getValue(), client.getStatusBundled());
     }
 
 }
