4b984fb6c1a0ec0801708ee142be38bb844fa0a8
Przemyslaw Beskosty <beskosty@gmail.com>
2020-06-12 17:35:45 +0200

    Unit test for Software. Test for getAmountPeriodBundled() method.

--- MOSC/src/test/java/pl/beskosty/mosc/entity/TestContract.java
+++ MOSC/src/test/java/pl/beskosty/mosc/entity/TestContract.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import pl.beskosty.mosc.enums.AmountPeriod;
 import pl.beskosty.mosc.enums.Status;
 
 /**
@@ -12,6 +13,8 @@ import pl.beskosty.mosc.enums.Status;
  * @author Przemyslaw Beskosty
  */
 public class TestContract {
+
+    private final String INCORRECT_AMOUNT_PERIOD = "WEEK";
 
     @Test
     public void isActiveOnConstructor() {
@@ -79,6 +82,98 @@ public class TestContract {
         contract.setStatus("A");
         //Then
         assertEquals(Status.ACTIVE.getValue(), contract.getStatusBundled());
+    }
+
+    @Test
+    public void checkNullOnAmountPeriodBundledOnConstructor() {
+        //Given
+        Contract contract = new Contract();
+        //Then
+        assertNotEquals(null, contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledOnConstructorAsMonth() {
+        //Given
+        Contract contract = new Contract();
+        //Then
+        assertNotEquals(AmountPeriod.MONTHLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledWithIncorrectAmountPeriodAsMonth() {
+        //Given
+        Contract contract = new Contract();
+        //When
+        contract.setAmountPeriod(INCORRECT_AMOUNT_PERIOD);
+        //Then
+        assertNotEquals(AmountPeriod.MONTHLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledWithCorrectAmountPeriodAsMonth() {
+        //Given
+        Contract contract = new Contract();
+        //When
+        contract.setAmountPeriod("MONTH");
+        //Then
+        assertEquals(AmountPeriod.MONTHLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledOnConstructorAsQuarter() {
+        //Given
+        Contract contract = new Contract();
+        //Then
+        assertNotEquals(AmountPeriod.QUARTERLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledWithIncorrectAmountPeriodAsQuarter() {
+        //Given
+        Contract contract = new Contract();
+        //When
+        contract.setAmountPeriod(INCORRECT_AMOUNT_PERIOD);
+        //Then
+        assertNotEquals(AmountPeriod.QUARTERLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledWithCorrectAmountPeriodAsQuarter() {
+        //Given
+        Contract contract = new Contract();
+        //When
+        contract.setAmountPeriod("QUARTER");
+        //Then
+        assertEquals(AmountPeriod.QUARTERLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledOnConstructorAsYear() {
+        //Given
+        Contract contract = new Contract();
+        //Then
+        assertNotEquals(AmountPeriod.YEARLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledWithIncorrectAmountPeriodAsYear() {
+        //Given
+        Contract contract = new Contract();
+        //When
+        contract.setAmountPeriod(INCORRECT_AMOUNT_PERIOD);
+        //Then
+        assertNotEquals(AmountPeriod.YEARLY.getValue(), contract.getAmountPeriodBundled());
+    }
+
+    @Test
+    public void checkAmountPeriodBundledWithCorrectAmountPeriodAsYear() {
+        //Given
+        Contract contract = new Contract();
+        //When
+        contract.setAmountPeriod("YEAR");
+        //Then
+        assertEquals(AmountPeriod.YEARLY.getValue(), contract.getAmountPeriodBundled());
     }
 
 }
